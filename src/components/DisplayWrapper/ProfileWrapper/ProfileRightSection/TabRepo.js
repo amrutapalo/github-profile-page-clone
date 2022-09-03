d9b4cf4b3d71539eeeba25e5edb2e0548534c759
amrutapalo <amrutapalo1997@gmail.com>
2022-09-04 05:01:20 +0530

    repository-tab

--- src/components/DisplayWrapper/ProfileWrapper/ProfileRightSection/TabRepo.js
+++ src/components/DisplayWrapper/ProfileWrapper/ProfileRightSection/TabRepo.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from "react-redux";
+import TabRepositoryCard from "./TabRepositoryCard";
+import "./TabRepo.css";
 
 const TabRepo = () => {
+  const userData = useSelector((state) => state);
+  console.log(userData);
   return (
-    <div>TabRepo</div>
-  )
+    <div className="repository-tab-container">
+      <div className="repository-tab-RepositoryList">
+        {userData.userRepoReducer.data.length !== 0 ? (
+          userData.userRepoReducer.data.map((repo) => {
+            return (
+              <TabRepositoryCard
+                name={repo.name}
+                visibility={repo.private ? "private" : "public"}
+                language={repo.language}
+                description={repo.description}
+                forks={repo.forks_count}
+                stars={repo.watchers}
+              ></TabRepositoryCard>
+            );
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default TabRepo;
